cee20cb43affcd5434f7991c694bb7c4d2e97803
Yago Diniz <70608174+Yagodiniz@users.noreply.github.com>
2022-09-21 15:51:15 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,39 @@
+import java.util.UUID;
+
+public class Aluno {
+    private String nome, endereco;
+    private int idade;
+    private UUID uuid;
+       
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
